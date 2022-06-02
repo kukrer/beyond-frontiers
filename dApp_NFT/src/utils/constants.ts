1c9a750bb8d2950423af9760a5764e3c91dcb94e
Volkan Kukrer <volkan.kukrer@gmail.com>
2022-06-02 16:01:07 +0200

    polygon mumbai contract

--- dApp_NFT/src/utils/constants.ts
+++ dApp_NFT/src/utils/constants.ts
@@ -1,5 +1,5 @@
 import nft from './NFT.contract.json';
 
 // NFT contract goes here
-export const contractNFTAddress = '0x449EDc4Da467B170c7825AD66434dee29619656E';
+export const contractNFTAddress = '0x9EC65E9b3cd0d7eF61a317a34fBF0F609905F782';
 export const contract_NFT_ABI = nft.abi;
